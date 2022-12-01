673ad900b5a5628320b8cac956dc1e3323366da3
Hill2929 <736120@student.dusd.net>
2022-12-01 15:24:55 -0800

    Added Box Collider 2D
    Added 2D box collider to everything

--- RubysAdventureRosasP7/Assets/RubyController.cs
+++ RubysAdventureRosasP7/Assets/RubyController.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical; 
+    public float speed = 3.0f; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //create variables to use the built in axis
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }   
+
+    void FixedUpdate ()
+    { 
         //Create movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //make horizontal and vertical movement work 
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime; 
-        transform.position = position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
